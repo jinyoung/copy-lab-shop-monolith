bbeba611068743a468f7ddec54da62474429c61f
Jinyoung Jang <jyjang@uengine.org>
2022-08-18 13:42:23 +0900

    commit msg

---
+++ monolith/src/main/java/copylabshopmonolith/domain/Order.java
@@ -0,0 +1,42 @@
+package copylabshopmonolith.domain;
+
+import copylabshopmonolith.MonolithApplication;
+import copylabshopmonolith.domain.OrderPlaced;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productId;
+
+    private Integer qty;
+
+    private String customerId;
+
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    @PrePersist
+    public void onPrePersist() {}
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = MonolithApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
---
+++ monolith/src/test/java/copylabshopmonolith/common/CucumberSpingConfiguration.java
@@ -0,0 +1,9 @@
+package copylabshopmonolith.common;
+
+import copylabshopmonolith.MonolithApplication;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = { MonolithApplication.class })
+public class CucumberSpingConfiguration {}
